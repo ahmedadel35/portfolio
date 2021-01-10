619883bab1c0596ec89bb6bce8f02205f5ac4717
abo3adel <abo3adel35@gmail.com>
2021-01-10 14:00:49 +0000

    purge fonts

--- purgecss.config.js
+++ purgecss.config.js
@@ -5,8 +5,11 @@ module.exports = {
         'pages/**/*.vue',
         'plugins/**/*.js',
         'plugins/**/*.ts',
+        'lost_classes.html',
     ],
-    css: ['static/css/bootstrap.min.css'],
     safelist: ['body', 'html', 'nuxt-progress'],
+    css: ['static/css/bootstrap.min.css'],
     output: 'static/css/app.css',
+    // css: ['static/css/fontawesome.min.css'],
+    // output: 'static/css/fontawesome.css',
 }
